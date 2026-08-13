1c69c7c801a3d8838be68c7df5a166b0b38fc6f0
sgeangola <correiamufuati@gmail.com>
2026-08-13 19:10:59 +0100

    Update gestor-login.js

--- src/services/gestor-login.js
+++ src/services/gestor-login.js
@@ -2,32 +2,44 @@ import { login } from "./auth.js";
 
 const form = document.getElementById("loginForm");
 
+
 form.addEventListener("submit", async (event) => {
 
     event.preventDefault();
 
+
     const email =
-        document.getElementById("email").value;
+    document.getElementById("email").value;
+
 
     const password =
-        document.getElementById("password").value;
+    document.getElementById("password").value;
+
+
 
     const result =
-        await login(email, password);
+    await login(email, password);
+
+
 
     if(result.success){
 
-        alert("Login efetuado com sucesso!");
 
-        window.location.href =
-            "../pages/dashboard-gestor.html";
+        alert("Login efetuado com sucesso!");
+        
+
+window.location.href = "../pages/dashboard-gestor.html";
+
 
     }else{
 
+
         alert(
-            "Erro no login: " + result.message
+        "Erro no login: " + result.message
         );
 
+
     }
+
 
 });
