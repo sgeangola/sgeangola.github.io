4c2460b7561475824524cd2898f8cb5bfd6625df
sgeangola <correiamufuati@gmail.com>
2026-08-16 08:23:24 +0100

    Update mini-pauta.js

--- src/services/mini-pauta.js
+++ src/services/mini-pauta.js
@@ -6,7 +6,7 @@
 // Separação por escola
 // =====================================================
 
-alert("MINI-PAUTA.JS 12CARREGADO ✅");
+alert("MINI-PAUTA.JS CARREGADO ✅");
 
 import { db } from "./firebase.js";
 
