0b1f91f5d3cbf78e8d213fe990757da39434c419
sgeangola <correiamufuati@gmail.com>
2026-08-18 08:32:49 -0300

    Update notas.js

--- src/services/notas.js
+++ src/services/notas.js
@@ -14,7 +14,7 @@
 // turmaId + disciplina + trimestre
 // =====================================================
 
-alert("🔥 NOTAS.JS1 CARREGADO!");
+alert("🔥 NOTAS.JS 10 CARREGADO!");
 
 // =====================================================
 // FIREBASE
