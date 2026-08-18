e94620f9838488f9a62b6c182bbd4f004ff574c2
sgeangola <correiamufuati@gmail.com>
2026-08-18 18:19:06 -0300

    Update notas.js

--- src/services/notas.js
+++ src/services/notas.js
@@ -15,3 +15,51 @@ import {
 alert("🔥 NOTAS.JS CARREGOU!");
 
 alert("🔥 FIREBASE E FIRESTORE IMPORTADOS!");
+
+// =====================================================
+// ESCOLA
+// =====================================================
+
+const escolaId =
+    sessionStorage.getItem("escolaId") ||
+    localStorage.getItem("escolaId");
+
+alert(
+    "🔥 ESCOLA: " +
+    (escolaId || "NÃO ENCONTRADA")
+);
+
+
+// =====================================================
+// ELEMENTOS
+// =====================================================
+
+const filtroProfessor =
+    document.getElementById("filtroProfessor");
+
+const filtroClasse =
+    document.getElementById("filtroClasse");
+
+const filtroTurma =
+    document.getElementById("filtroTurma");
+
+const filtroDisciplina =
+    document.getElementById("filtroDisciplina");
+
+const filtroTrimestre =
+    document.getElementById("filtroTrimestre");
+
+const notasLista =
+    document.getElementById("notasLista");
+
+const mensagem =
+    document.getElementById("mensagem");
+
+const estadoSistema =
+    document.getElementById("estadoSistema");
+
+const botaoSistema =
+    document.getElementById("botaoSistema");
+
+
+alert("🔥 ELEMENTOS DA PÁGINA CARREGADOS!");
